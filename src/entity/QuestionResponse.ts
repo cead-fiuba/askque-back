25370e231da74f354283fc95eb68f907d36ae094
crisnicogonzalez <cristian3629@gmail.com>
2020-07-04 17:04:59 -0300

    delete questionary is ok

--- src/entity/QuestionResponse.ts
+++ src/entity/QuestionResponse.ts
@@ -1,26 +1,25 @@
 import { Response } from "./Response";
 import { ManyToOne, PrimaryGeneratedColumn, Entity, Column } from "typeorm";
 
-
 /**
- * 
+ *
  * Las options que se encuentran aca son las que fueron marcadas por el alumno para la pregunta dada
- * 
+ *
  */
 
 @Entity()
 export class QuestionResponse {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  questionId: number;
 
-    @Column()
-    questionId: number
+  @Column("simple-array")
+  optionIds: number[];
 
-    @Column("simple-array")
-    optionIds: number[]
-
-    @ManyToOne(type => Response, response => response.question_responses)
-    response: Response
-
+  @ManyToOne((type) => Response, (response) => response.question_responses, {
+    onDelete: "CASCADE",
+  })
+  response: Response;
 }
--- src/entity/Questionary.ts
+++ src/entity/Questionary.ts
@@ -1,51 +1,62 @@
-import { Entity, Column, OneToMany, ManyToOne, PrimaryColumn, JoinTable } from "typeorm";
+import {
+  Entity,
+  Column,
+  OneToMany,
+  ManyToOne,
+  PrimaryColumn,
+  JoinTable,
+} from "typeorm";
 import { Question } from "./Question";
-import { Response } from "./Response"
+import { Response } from "./Response";
 import { Teacher } from "./Teacher";
-
 
 @Entity()
 export class Questionary {
+  @PrimaryColumn()
+  hash: string;
 
-    @PrimaryColumn()
-    hash: string
+  @Column()
+  name: string;
 
-    @Column()
-    name: string;
+  @Column("text", { nullable: true })
+  description: string;
 
-    @Column("text", { nullable: true })
-    description: string
+  @Column()
+  time_in_minutes: number;
 
-    @Column()
-    time_in_minutes: number;
+  @OneToMany((type) => Question, (q) => q.questionary, {
+    eager: true,
+    cascade: ["insert", "update"],
+    onDelete: "CASCADE",
+  })
+  questions: Question[];
 
-    @OneToMany(type => Question, q => q.questionary, { eager: true, cascade: ["insert", "update"] })
-    questions: Question[]
+  @OneToMany((type) => Response, (response) => response.questionary, {
+    onDelete: "CASCADE",
+  })
+  responses: Response[];
 
-    @OneToMany(type => Response, response => response.questionary)
-    responses: Response[]
+  @ManyToOne((type) => Teacher, (u) => u.createdQuestionaries)
+  teacher: Teacher;
 
-    @ManyToOne(type => Teacher, u => u.createdQuestionaries)
-    teacher: Teacher
+  @Column()
+  date: Date;
 
-    @Column()
-    date: Date
+  @Column()
+  module: string;
 
-    @Column()
-    module: string
+  @Column("int2", { nullable: true, default: 0 })
+  quantity_respones: number;
 
-    @Column('int2', { nullable: true, default: 0 })
-    quantity_respones: number
+  @Column({ nullable: true })
+  teacher_name: string;
 
-    @Column({ nullable: true })
-    teacher_name: string
+  @Column({ nullable: true })
+  user_name: string;
 
-    @Column({ nullable: true })
-    user_name: string
+  @Column("int2", { nullable: true })
+  semester: number;
 
-    @Column('int2', { nullable: true })
-    semester: number
-
-    @Column({ default: false })
-    can_show_result: boolean
+  @Column({ default: false })
+  can_show_result: boolean;
 }
